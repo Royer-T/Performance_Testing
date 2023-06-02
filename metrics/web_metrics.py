ccddc4d8c6ddf01e147ee0e6c61ea81365d18d8c
Roger Thibodeau <RRJThibodeau@msn.com>
2023-06-02 15:34:52 -0700

    class optimizations

--- metrics/web_metrics.py
+++ metrics/web_metrics.py
@@ -1,89 +1,79 @@
 import constants
-import json
 import pycurl
-import time
 from io import BytesIO
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium_navigation.webdriver import ChromeDriver
 
 
 class WebMetrics:
     def __init__(self, url):
+        """
+        Initializes a WebMetrics object with the specified URL.
+
+        :param: url (str) The URL to measure the web metrics for.
+        """
         self.url = url
 
     def calculate_ttfb(self):
         """
-        Calculates Time To First Byte (TTFB) by making an HTTP request to
-        the specified URL.
+        Calculates the Time To First Byte (TTFB) for the specified URL.
 
-        :parameter: URL where the TTFB is to be calculated (str)
-        :return: A JSON string containing the following metrics:
-        - 'dns_lookup': Time taken in milliseconds to perform the DNS lookup.
-        - 'connect_time': Time taken in milliseconds to establish the
-        connection.
-        - 'start_transfer_time': Time taken in milliseconds to receive the first
-        byte.
-        - 'total_time': Total time in milliseconds taken for the entire HTTP
-        request.
-        :rtype: str (a JSON string)
+        :param: The URL to measure the web metrics for
+        :return: A dictionary containing the following TTFB metrics in
+                milliseconds:
+                - 'dns_lookup': Time taken for DNS lookup
+                - 'connect_time': Time taken to establish the connection
+                - 'start_transfer_time': Time taken to receive the first byte
+                - 'total_time': Total time taken for the entire HTTP request
+        :rtype:  dict
         """
-        # create and instance of 'Curl' from the 'pycurl' module
         curl = pycurl.Curl()
+        try:
+            curl.setopt(pycurl.URL, self.url)
+            buffer = BytesIO()
+            curl.setopt(curl.WRITEFUNCTION, buffer.write)
+            curl.setopt(pycurl.FOLLOWLOCATION, 1)
+            curl.perform()
 
-        # specifies the URL to connect to when making an HTTP request
-        curl.setopt(pycurl.URL, self.url)
+            # gather some information (we are going to want time in ms)
+            # 1. time taken in milliseconds to perform the DNS lookup
+            dns_time = curl.getinfo(pycurl.NAMELOOKUP_TIME)
+            ms_dns_time = round((dns_time * 1000), 0)
 
-        # Create a buffer to store the response
-        # - this is used to prevent unneeded console writes to help with
-        # potential debugging
-        buffer = BytesIO()
+            # 2. time it took in milliseconds to establish the connection
+            connect_time = curl.getinfo(pycurl.CONNECT_TIME)
+            ms_connect_time = round((connect_time * 1000), 0)
 
-        # Set the buffer as the write function for curl.perform()
-        curl.setopt(curl.WRITEFUNCTION, buffer.write)
+            # 3. time it took in milliseconds to receive the first byte (TTFB)
+            start_transfer_time = curl.getinfo(pycurl.STARTTRANSFER_TIME)
+            ms_start_transfer_time = round((start_transfer_time * 1000), 0)
 
-        #  automatically follow HTTP redirects until it reaches the final
-        #  destination URL
-        curl.setopt(pycurl.FOLLOWLOCATION, 1)
+            # 4. total time in milliseconds taken for the entire HTTP request,
+            # from the time Curl initiated the request until the entire response
+            # was received.
+            total_time = curl.getinfo(pycurl.TOTAL_TIME)
+            ms_total_time = round((total_time * 1000), 0)
 
-        #  execute the HTTP request
-        curl.perform()
+            curl_metrics = {
+                'dns_lookup': ms_dns_time,
+                'connect_time': ms_connect_time,
+                'start_transfer_time': ms_start_transfer_time,
+                'total_time': ms_total_time
+            }
 
-        # gather some information (we are going to want time in ms)
-        # 1. time taken in milliseconds to perform the DNS lookup
-        dns_time = curl.getinfo(pycurl.NAMELOOKUP_TIME)
-        ms_dns_time = round((dns_time * 1000), 0)
-
-        # 2. time it took in milliseconds to establish the connection
-        connect_time = curl.getinfo(pycurl.CONNECT_TIME)
-        ms_connect_time = round((connect_time * 1000), 0)
-
-        # 3. time it took in milliseconds to receive the first byte (TTFB)
-        start_transfer_time = curl.getinfo(pycurl.STARTTRANSFER_TIME)
-        ms_start_transfer_time = round((start_transfer_time * 1000), 0)
-
-        # 4. total time in milliseconds taken for the entire HTTP request,
-        # from the time Curl initiated the request until the entire response
-        # was received.
-        total_time = curl.getinfo(pycurl.TOTAL_TIME)
-        ms_total_time = round((total_time * 1000), 0)
-
-        # closes Curl instance and releases any resources associated with it
-        curl.close()
-
-        curl_metrics = json.dumps({'dns_lookup': ms_dns_time,
-                                   'connect_time': ms_connect_time,
-                                   'start_transfer_time': ms_start_transfer_time,
-                                   'total_time': ms_total_time})
-
-        return curl_metrics
+            return curl_metrics
+        finally:
+            curl.close()
 
     def calculate_page_load_time(self):
         """
-        Calculates the page load time for a given URL using Chrome WebDriver.
+        Calculates the page load time for the specified URL.
 
-        :return: The page load time in milliseconds
+        :param: The URL to measure the web metrics for
+        :return: The page load time in milliseconds.
         :rtype: int
-        :raise: WebDriverException: If an error occurs while starting the
-        Chrome browser or navigating to the URL.
         """
         #  start the browser/webdriver
         driver = ChromeDriver.start_chrome(constants.CHROME_EXE,
@@ -92,19 +82,13 @@ class WebMetrics:
         # navigate to the URL being evaluated
         driver.get(self.url)
 
-        # wait for the page to load
-        while driver.execute_script('return document.readyState') != 'complete':
-            time.sleep(0.1)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located((By.XPATH, '//body')))
 
-        # Get the page load time
-        navigation_start = driver.execute_script('return window.performance.'
-                                                 'timing.navigationStart')
-        load_event_end = driver.execute_script('return window.performance.'
-                                               'timing.loadEventEnd')
+        navigation_start = driver.execute_script('return window.performance.timing.navigationStart')
+        load_event_end = driver.execute_script('return window.performance.timing.loadEventEnd')
         page_load_time = load_event_end - navigation_start
 
-        # quit the browser
         driver.quit()
 
-        # Return the page load time
         return page_load_time
--- websites/website.py
+++ websites/website.py
@@ -3,6 +3,11 @@ import requests
 
 class Website:
     def __init__(self, url):
+        """
+        Initialize a Website object with the provided URL.
+
+        :param url: The URL of the website being checked.
+        """
         self.url = url
 
     def website_up(self):
@@ -10,16 +15,13 @@ class Website:
         Check if a website is up and running.
 
         :parameter: URL being checked
-        :return: if the website is up and the response status code is between
-        200 and 399 (inclusive)
+        :return: True if the website is up and the response status code is between
+        200 and 399 (inclusive), False otherwise
         :rtype: bool
         """
         try:
             response = requests.get(self.url)
-
-            if 200 <= response.status_code < 400:
-                return True
-            else:
-                return False
+            return 200 <= response.status_code < 400
         except requests.exceptions.RequestException:
             return False
+
